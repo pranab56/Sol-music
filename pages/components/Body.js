c85506e6f25d27b992d61b531a06836b0f3dae03
pranab56 <pronabhalder53@gmail.com>
2022-08-04 20:11:30 +0600

    search done

--- pages/components/Body.js
+++ pages/components/Body.js
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import Search from "./Search";
 
 const Body = () => {
-    return (
-        <section>
-            <Search/>
-        </section>
-    );
+  const [search, setSearch] = useState("");
+  return (
+    <section className="bg-black ml-24 py-4 space-y-8 md:max-w-4xl rounded-md flex-grow md:mr-2.5">
+      <Search search={search} setSearch={setSearch} />
+    </section>
+  );
 };
 
 export default Body;
