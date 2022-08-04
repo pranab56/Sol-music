ecb267975f4a3038006a214c46a7f37e8f054e7b
pranab56 <pronabhalder53@gmail.com>
2022-08-04 18:50:01 +0600

    search implement

--- pages/components/Search.js
+++ pages/components/Search.js
@@ -1,7 +1,7 @@
 const Search = () => {
     return (
-        <div>
-            <h1>this is search</h1>
+        <div className="max-w-[1150px] bg-[#1a1a1a] rounded-full overflow-hidden border-2 border-[#333333] p-1.5 px-5 pr-8 flex items-center">
+            <div className="h-4 w-4 rounded-full border-2 flex-shrink-0 animate-pulse "/>
         </div>
     );
 };
