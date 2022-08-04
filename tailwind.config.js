9b281ef4b2cc2039d2c13d1afd83d300fde962f6
pranab56 <pronabhalder53@gmail.com>
2022-08-05 00:09:02 +0600

    search btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,34 @@ module.exports = {
   },
 
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1500px",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0.5,
+          },
+        },
+      },
+    },
+    animation: {
+      pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+    },
   },
+
+  variants: {
+    extend: {},
+    scrollbar: ["rounded"],
+  },
+  
   plugins: [
     require("tailwind-scrollbar-hide"),
     require("@tailwindcss/forms"),
     require("tailwind-scrollbar"),
   ],
-}
+};
